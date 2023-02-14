43f3ea471719ec9d90b3e9e68e5c8e5265f04661
tubayakar <tubakevserykr@gmail.com>
2023-02-15 00:06:37 +0300

    lwc

---
+++ force-app/main/default/lwc/accountComponent/accountComponent.js
@@ -0,0 +1,58 @@
+import { LightningElement,wire,track } from 'lwc';
+
+ // import apex classs method.
+
+import retrive from '@salesforce/apex/accountLWCController.retrive';
+
+export default class AccountComponent extends LightningElement {
+
+    // properties
+    @track multiaccounts;
+
+    searchText;
+
+
+    // wire
+    // @wire(retrive,{searchterm:'$searchText'}) accounts;
+
+    renderedCallback(){
+        console.log(this.accounts);
+    }
+
+
+    textChangeHandler(event){
+
+        this.searchText=event.target.value;
+        console.log(this.searchText);
+    
+
+        // imperative
+
+        retrive({
+            searchterm:this.searchText
+        })
+
+
+        .then(data=>{
+            console.log('retrived Accounts');
+            console.log(data);
+            this.multiaccounts=data;
+
+        })
+
+        .catch(err=>{
+            console.log(err);
+
+        })
+
+    }
+
+    connectedCallback(){
+
+    console.log('connected callback is running...');
+
+   }
+   
+    
+
+}
