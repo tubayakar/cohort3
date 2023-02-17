ee92ad64b6f06d6166006ef4e9282e69c0c16c56
tubayakar <tubakevserykr@gmail.com>
2023-02-18 00:14:58 +0300

    lwc

---
+++ force-app/main/default/lwc/messageSubscriber/messageSubscriber.js
@@ -0,0 +1,77 @@
+import { LightningElement,wire } from 'lwc';
+
+
+import {
+  subscribe,
+  unsubscribe,
+  APPLICATION_SCOPE,
+  MessageContext
+} from 'lightning/messageService';
+
+
+import mc from '@salesforce/messageChannel/mc__c'; // mc.messageChannel-meta.xml
+
+
+
+export default class MessageSubscriber extends LightningElement {
+
+  datareceived;
+
+
+  @wire(MessageContext)
+    messageContext;
+
+
+
+    subscribeToMessageChannel() {
+      if (!this.subscription) {
+          this.subscription = subscribe(
+              this.messageContext,
+              mc,
+              (message) => this.handleMessage(message),
+              { scope: APPLICATION_SCOPE }
+          );
+      }
+  }
+
+
+// Handler for message received by component
+handleMessage(message) {
+  console.log('message received ');
+  console.log(message);
+
+
+  this.datareceived = message.recordId;
+
+}
+
+
+unsubscribeToMessageChannel() {
+  unsubscribe(this.subscription);
+  this.subscription = null;
+}
+
+
+
+connectedCallback() {
+
+    console.log('connectedCallback is running...');
+  this.subscribeToMessageChannel();
+
+
+   
+}
+
+
+disconnectedCallback() {
+    console.log('disconnectedCallback is running...');
+  this.unsubscribeToMessageChannel();
+}
+
+
+
+renderedCallback(){
+    console.log('renderedCallback is running...');
+}
+
+}
