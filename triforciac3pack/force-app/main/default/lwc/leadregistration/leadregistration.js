2843e0fac425317f022d51ba374ced26551f8145
tubayakar <tubakevserykr@gmail.com>
2023-02-23 00:03:05 +0300

    lwc

--- triforciac3pack/force-app/main/default/lwc/leadregistration/leadregistration.js
+++ triforciac3pack/force-app/main/default/lwc/leadregistration/leadregistration.js
@@ -1,7 +1,7 @@
 import { LightningElement } from 'lwc';
 
 export default class Leadregistration extends LightningElement {
-    
+
 singleLead={
   FirstName:'',
   LastName:'',
@@ -15,5 +15,34 @@ singleLead={
     inputChangeHandler(event){
     console.log(event.target.name);
     console.log(event.target.value);
-    }
+  
+  switch(event.target.name) {
+    case 'FirstName':
+      this.singleLead.FirstName=event.target.value;
+      break;
+    case 'LastName':
+      this.singleLead.LastName=event.target.value;
+      break;
+    case 'Phone':
+      this.singleLead.Phone=event.target.value;
+      break;
+    case 'Email':
+      this.singleLead.Email=event.target.value;
+      break;
+    case 'Country':
+      this.singleLead.Country=event.target.value;
+      break;
+    case 'Info_Session_Date_Time__c':
+      this.singleLead.Info_Session_Date_Time__c=event.target.value;
+      break;
+  
+    default:
+      // code block
+      console.log('default....');
+  }
+
+  console.log(JSON.parse(JSON.stringify(this.singleLead)));
+
+}
+
 }
