0971752dc2d04a93f24bcbe2a8eb45e4291a7d45
Tuba Yakar <110194596+tubayakar@users.noreply.github.com>
2023-03-02 23:16:36 +0300

    Update oppcomponent.js

--- force-app/main/default/lwc/oppcomponent/oppcomponent.js
+++ force-app/main/default/lwc/oppcomponent/oppcomponent.js
@@ -1,1 +1,56 @@
+import { LightningElement, track } from 'lwc';
 
+
+//import apex class method.
+import retrive from '@salesforce/apex/oppLWCController.retrive';
+
+
+const columns = [
+    { label: 'Opporunity Name', fieldName: 'Name' },
+    { label: 'Amount', fieldName: 'Amount', type: 'currency' },
+    { label: 'Stage', fieldName: 'StageName', type: 'picklist' },
+    { label: 'Close Date', fieldName: 'CloseDate', type: 'date' },
+    { label: 'Type', fieldName: 'Type', type: 'picklist' },
+];
+
+
+export default class Oppcomponent extends LightningElement {
+
+@track multiopp;
+searchText;
+columns = columns;
+
+renderedCallback(){
+    console.log(this.opportunities);
+}
+
+textChangeHandler(event){
+    this.searchText=event.target.value;
+    console.log(this.searchText);
+
+    //imperative
+
+    retrive({
+        searchterm:this.searchText
+    })
+
+    .then(data=>{
+        console.log('retrived Opportunities');
+        console.log(data);
+        this.multiopp=data;
+    })
+
+    .catch(err=>{
+        console.log(err);
+    })
+
+}
+
+
+
+connectedCallback() {
+    console.log('connected callback is running...')
+}
+
+
+}
